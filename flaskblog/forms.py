a76c63ddc049120b1e05385a2579d129bd4f8c3e
NathanShepard <45462632+NathanShepard@users.noreply.github.com>
2022-11-12 11:16:20 -0500

    first commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,64 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:  # if none no Error it more then none. thows an error
+            raise ValidationError('That username is taken. Please choose another name.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:  # if none no Error it more then none. thows an error
+            raise ValidationError('That email is taken. Please choose another name.')
+
+
+class LoginForm(FlaskForm):
+    # field
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Rememder Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:  # if none no Error it more then none. thows an error
+                raise ValidationError('That username is taken. Please choose another name.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:  # if none no Error it more then none. thows an error
+                raise ValidationError('That email is taken. Please choose another name.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
---
+++ flaskblog/templates/index.js
@@ -0,0 +1,22 @@
+
+/* Data Types:
+undefined, null, boolean, string, symbol, numder, and object
+*/
+
+var myName = "Nathan"
+
+myName = 8
+
+var a;
+
+console.log(a) 
+
+var b = 2;
+
+a = 7;
+
+b = 7;
+
+console.log(a)
+
+
