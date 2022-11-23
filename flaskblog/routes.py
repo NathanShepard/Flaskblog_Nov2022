6a4a6f0c1e88864f27c7233a64b304cf9e944525
NathanShepard <45462632+NathanShepard@users.noreply.github.com>
2022-11-23 17:55:29 -0500

    pagnation last=first

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,6 +12,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
+    print('this is a TEST >>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
@@ -151,7 +152,7 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 
-@app.route("/user/<str:username>")
+@app.route("/user/<string:username>")
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
